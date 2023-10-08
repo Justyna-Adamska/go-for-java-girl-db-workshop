42706a51c3d1995ef7e797b4f17e5d508be38e44
Justyna Adamska <justyna.anna.adamska@gmail.com>
2023-10-08 08:41:09 +0200

    Workshop changes

--- src/main/java/org/goforjava/db/DepartmentDB.java
+++ src/main/java/org/goforjava/db/DepartmentDB.java
@@ -4,6 +4,7 @@ import org.goforjava.domain.Department;
 import org.goforjava.domain.Id;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DepartmentDB implements DB<Department> {
 
@@ -11,12 +12,18 @@ public class DepartmentDB implements DB<Department> {
 
     @Override
     public List<Department> findAll() {
-        return List.of();
+        return  departmentMap.values().stream().collect(Collectors.toList());
     }
 
     @Override
     public Optional<Department> findById(Id id) {
-        return Optional.empty();
+
+        var department = departmentMap.get(id);
+        if(department!=null){
+            return Optional.of(department);
+        }
+        else{
+        return Optional.empty();}
     }
 
     @Override
